72887a450bc0d43b1aaff3409a4ddbedd6658517
Idel Pivnitskiy <Idel.Pivnitskiy@gmail.com>
2015-03-09 22:52:17 +0300

    #18 Added logger (log4j2)

--- 18-ioc/src/main/java/ua/pp/condor/ioc/Main.java
+++ 18-ioc/src/main/java/ua/pp/condor/ioc/Main.java
@@ -1,8 +1,13 @@
 package ua.pp.condor.ioc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Main {
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("work");
+        log.info("work");
     }
 }
