d5d7c46a27bf18a6c8461e634145bbeee8edf913
Idel Pivnitskiy <Idel.Pivnitskiy@gmail.com>
2015-03-11 00:54:10 +0300

    #18 Added test environment

---
+++ 18-ioc/src/test/java/ua/pp/condor/ioc/config/TestConfiguration.java
@@ -0,0 +1,23 @@
+package ua.pp.condor.ioc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@PropertySource("classpath:hibernate.properties")
+@Import(ApplicationConfiguration.class)
+public class TestConfiguration {
+
+    @Bean(destroyMethod = "shutdown")
+    EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .build();
+    }
+}
